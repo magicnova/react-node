1aed5eae21cbe043d308c2a14bf878a147451163
magicnova@hotmail.com <your-correct-email@example.com>
2018-05-02 17:26:19 -0300

    refactor : added route file and passport service

--- server/index.js
+++ server/index.js
@@ -1,30 +1,8 @@
 const express = require("express");
-const passport = require("passport");
-
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
-
 const app = express();
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientId,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: "/auth/google/callback",
-		},
-		(accessToken, refreshToken, profile, done) => {},
-	),
-);
-
-app.get(
-	"/auth/google",
-	passport.authenticate("google", {
-		scope: ["profile", "email"],
-	}),
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
-
 const PORT = process.env.PORT || 5000;
+
+require("./services/passportService");
+require("./routes/authRoute")(app);
+
 app.listen(PORT);
