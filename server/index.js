bde9358208f5db05f2b4e150bfbca0e5dbb8a60f
magicnova@hotmail.com <your-correct-email@example.com>
2018-05-02 17:39:31 -0300

    added mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURI);
 
 require("./services/passportService");
 require("./routes/authRoute")(app);
