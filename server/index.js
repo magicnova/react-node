7cf34adba2cf1085981fab2cbf3f65dd53b4d1fd
magicnova@hotmail.com <your-correct-email@example.com>
2018-05-23 17:16:38 -0300

    Add cookie session new routes for login / logout

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(keys.mongoURI);
 require("./models/User");
 require("./services/passportService");
+
+mongoose.connect(keys.mongoURI);
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
+	}),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 require("./routes/authRoute")(app);
 
 app.listen(PORT);
